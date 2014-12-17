703cd2df7c23d82975ad4e4734028691069a4681
Vitali Shypko <vshypko@gmail.com>
2014-12-17 14:38:23 -0800

    mobile-menu links fix

--- js/script.js
+++ js/script.js
@@ -4,16 +4,14 @@
 	var mobileMenu = document.querySelectorAll(".mobile-menu");
 	var header = document.querySelectorAll(".page-header__logo");
 
-	openMenu[0].addEventListener("click", function(event){
+	openMenu[0].addEventListener("click", function(){
 		if ((getComputedStyle(mobileMenu[0]).display) === "none") {
 			mobileMenu[0].style.display = "block";
 			header[0].style.backgroundColor = "#283645";
-			header[0].style.borderBottom = "1px solid #000";
 		}
 		else {
 			mobileMenu[0].style.display = "none";
 			header[0].style.backgroundColor = "rgba(0,0,0,.3)";
-			header[0].style.borderBottom = "none";
 		}
 	})
 	
