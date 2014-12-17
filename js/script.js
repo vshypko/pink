d65169b32c9829ea104324b5799f1728d107ecbf
Vitali Shypko <vshypko@gmail.com>
2014-12-17 12:35:04 -0800

    mobile-menu open and close

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-script
+(function() {
+
+	var openMenu = document.querySelectorAll(".js-menu-open");
+	var mobileMenu = document.querySelectorAll(".mobile-menu");
+	var header = document.querySelectorAll(".page-header__logo");
+
+	openMenu[0].addEventListener("click", function(event){
+		if ((getComputedStyle(mobileMenu[0]).display) === "none") {
+			mobileMenu[0].style.display = "block";
+			header[0].style.backgroundColor = "#283645";
+			header[0].style.borderBottom = "1px solid #000";
+		}
+		else {
+			mobileMenu[0].style.display = "none";
+			header[0].style.backgroundColor = "rgba(0,0,0,.3)";
+			header[0].style.borderBottom = "none";
+		}
+	})
+	
+})();
